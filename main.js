2bacd291d566f5fc08c2ed7e67c65e04aec4f541
shubham singh bohara <shubhambohara09@gmail.com>
2020-11-11 18:35:27 +0530

    resume prompt made

--- main.js
+++ main.js
@@ -1,7 +1,51 @@
 setInterval(function(){ splatStack.push(parseInt(Math.random() * 20) + 2); }, 10000);
 
 function resume(){
-    alert('this');
+  var dialog = bootbox.dialog({
+    title: 'Alright! looks like someone wanna hair me',
+    message: "<p>So what would you like to do with my resume?</p>",
+    size: 'large',
+    buttons: {
+      view: {
+        label: "view",
+        className: 'btn-primary',
+        callback: function(){
+          window.open("https://ssbking.github.io/testhtml/");
+            console.log('view resume online in new tab');
+            // return false;
+        }
+    },
+        email: {
+            label: "email📧",
+            className: 'btn-warning',
+            callback: function(){
+                console.log('email button clicked!');
+                // function to send email
+                bootbox.prompt({
+                  title: "Who to send this email to?",
+                  message: "<p>email will be sent with a link to this very site</p>",
+                  inputType: 'email',
+                  callback: function (result) {
+                      // console.log(result);
+                      var subject = "?subject=resume of shubham singh bohara";
+                      var body = "&body=Hello there! you can review my resume cum profile page by visiting https%3A%2F%2Fssbking.github.io%2Fmobileserver%2F ";
+                      var reciver = "mailto:"+result+subject+body;
+                      console.log(reciver);
+                      window.location.href = reciver;
+                  }
+              });
+            }
+        },
+        ok: {
+            label: "downlode PDF",
+            className: 'btn-info',
+            callback: function(){
+                console.log('donlode the resume clicked');
+                window.location = '../resume/ssbking_github_io_testhtml.pdf';
+            }
+        }
+    }
+});
 }
 
 console.log('ignor LDRLLL10.png error| its from fluid canvas js');
